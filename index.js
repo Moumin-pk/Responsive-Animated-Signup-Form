07c0472e11536939506253a47be57e65c1708af6
Moumin Ahmad <116239910+Moumin-pk@users.noreply.github.com>
2023-09-02 13:08:50 +0500

    Signup Form

---
+++ index.js
@@ -0,0 +1,26 @@
+var pass = document.getElementById('pass');
+var flag =1; // 1 -> no error | 0 -> error 
+function check(elem){
+    if(elem.value.length > 0){
+        if(elem.value != pass.value){
+            document.getElementById('alert').innerText='Please enter the correct value';
+            flag = 0;
+        }else{
+            document.getElementById('alert').innerText=" ";
+            flag = 1;
+        }
+    }else{
+        document.getElementById('alert').innerText = 'plese enter confirm password'
+        flag = 0;
+        
+    }
+    
+}
+
+function validate(){
+    if(flag == 1){
+        return true;
+    }else{
+        return false;
+    }
+}
